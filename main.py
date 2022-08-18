cbdef0908ca941a1c355c6eee2891cd3e0707163
Antônio Marcos <antoniomarcos.souza2002@gmail.com>
2022-08-18 15:20:54 -0300

    fn to enumerate all property constraints on wdprop

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import requests, json, os
+from enum import Enum
+
+
+class PropertyConstraints(Enum):
+    Q52004125 = "allowed-entity-types constraint"
+    Q21510851 = "allowed qualifiers constraint"
+    Q21503247 = "item-requires-statement constraint"
+    Q53869507 = "property scope constraint"
+    Q21503250 = "type constraint"
 
 
 def wdQuery(wdProperty):
@@ -21,16 +30,33 @@ def wdQuery(wdProperty):
     response = requests.get(url, params={"format": "json", "query": SPARQL_query})
     data = response.json()
 
-    resultsPath = "./results"
     if not os.path.exists(resultsPath):
         os.mkdir(resultsPath)
 
     with open(f"{resultsPath}/{wdProperty}.json", "w") as outfile:
         json.dump(data, outfile, indent=4)
 
+
+def wdPropertyConstraints(wdProperty):
+    property_constraints = []
+    with open(f"{resultsPath}/{wdProperty}.json", "r") as property_file:
+        property_json = json.load(property_file)
+        print(f"Property {wdProperty} has the following property constraints:")
+        for item in property_json["results"]["bindings"]:
+            constraint_url = item.get("constraint_type").get("value")
+            constraint = constraint_url[constraint_url.rfind("/Q") + 1 :]
+            for item in PropertyConstraints:
+                if constraint == item.name and constraint not in property_constraints:
+                    property_constraints.append(constraint)
+                    print(f"◆ {item.value}")
+
+
 ### Global Variables
 resultsPath = "./results"
 
-# wdProperty = input('For which property would you like to generate a SHACL property shape?')
+# wdProperty = input(
+#     "For which property would you like to generate a SHACL property shape?\n> "
+# )
 wdProperty = "P1559"
 wdQuery(wdProperty)
+wdPropertyConstraints(wdProperty)
