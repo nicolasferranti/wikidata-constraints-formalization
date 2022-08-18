9f39706028b7bca23109ceddc8dabc74ab38167f
Antônio Marcos <antoniomarcos.souza2002@gmail.com>
2022-08-18 16:47:01 -0300

    generate type shacl constraint shape

--- main.py
+++ main.py
@@ -4,6 +4,24 @@ from enum import Enum
 ### Global Variables
 results_path = "./results"
 
+constraint_shacl = {
+    "Q21503250": """
+@prefix :        <http://example.org/>
+@prefix wdt:     <http://www.wikidata.org/prop/direct/>
+@prefix wd:      <http://www.wikidata.org/entity/>
+@prefix sh:      <http://www.w3.org/ns/shacl#>
+
+:$WD_PROPERTY$_TypeConstraintShape 
+    a sh:NodeShape ;
+    sh:targetSubjectsOf wdt:$WD_PROPERTY$;
+    sh:or (
+        $PROPERTY_SHACL$
+    ) ;
+    sh:severity sh:Violation .
+  """,
+}
+
+
 class PropertyConstraints(Enum):
     Q52004125 = "allowed-entity-types constraint"
     Q21510851 = "allowed qualifiers constraint"
@@ -52,10 +70,49 @@ def wdPropertyConstraints(wdProperty):
                 if constraint == item.name and constraint not in property_constraints:
                     property_constraints.append(constraint)
                     print(f"◆ {item.value}")
+    shacl_generate(wdProperty, property_json, property_constraints)
 
 
-### Global Variables
-resultsPath = "./results"
+def shacl_generate(wdProperty, property_json, property_constraints):
+    for item in property_constraints:
+        if item in constraint_shacl:
+            if item == "Q21503250":
+                constraint_shape = constraint_shacl[item]
+                constraint_shape = constraint_shape.replace("$WD_PROPERTY$", wdProperty)
+                for json_item in property_json["results"]["bindings"]:
+                    if (
+                        json_item.get("constraint_type").get("value")[
+                            json_item.get("constraint_type").get("value").rfind("/Q")
+                            + 1 :
+                        ]
+                        == item
+                        and json_item.get("pq_qualifiers").get("value")
+                        != "http://www.wikidata.org/prop/qualifier/P2309"
+                    ):
+                        value_list = (
+                            json_item.get("object_val").get("value").split(", ")
+                        )
+                        for value in value_list:
+                            property_shacl = """
+                            [ sh:property [
+                                    sh:path ([sh:zeroOrOnePath wdt:P31] [sh:zeroOrMorePath wdt:P279]) ;
+                                    sh:minCount 1 ;
+                                    sh:hasValue wd:$PROPERTY$ ;
+                                ]
+                            ]
+                            $PROPERTY_SHACL$
+                            """
+                            property_shacl = property_shacl.replace(
+                                "$PROPERTY$", value[value.rfind("/Q") + 1 :]
+                            )
+                            constraint_shape = constraint_shape.replace(
+                                "$PROPERTY_SHACL$", property_shacl
+                            )
+                        constraint_shape = constraint_shape.replace("$PROPERTY_SHACL$", "")
+                with open(f"{results_path}/shacl_shape_{wdProperty}_{item}.ttl", "w") as outfile:
+                    outfile.write(constraint_shape)
+                    outfile.close()
+
 
 # wdProperty = input(
 #     "For which property would you like to generate a SHACL property shape?\n> "
