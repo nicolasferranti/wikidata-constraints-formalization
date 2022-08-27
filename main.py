2990ac652ca855a35a0191dd0bf9ea0c82982f4f
Antônio Marcos <antoniomarcos.souza2002@gmail.com>
2022-08-26 22:43:30 -0300

    trying to add item-requires-statement

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests, json, os
 from enum import Enum
 
 ### Global Variables
-results_path = "./results"
+query_results = "./query results"
 
 constraint_shacl = {
     "Q21503250": """
@@ -18,6 +18,21 @@ constraint_shacl = {
         $PROPERTY_SHACL$
     ) ;
     sh:severity sh:Violation .
+  """,
+    "Q21503247": """
+@prefix :        <http://example.org/>
+@prefix wdt:     <http://www.wikidata.org/prop/direct/>
+@prefix wd:      <http://www.wikidata.org/entity/>
+@prefix sh:      <http://www.w3.org/ns/shacl#>
+
+:$WD_PROPERTY$_ItemRequiresStatementShape 
+	a sh:NodeShape ;
+	sh:targetSubjectsOf wdt:$WD_PROPERTY$ ;
+	sh:property [
+    	sh:path wdt:$PROP_CONSTRAINT$ ;
+        sh:minCount 1;
+        sh:in (wd:$ITEM_OF_PROP_CONSTRAINT$) ;
+  	] .
   """,
 }
 
@@ -50,16 +65,16 @@ def wdQuery(wdProperty):
     response = requests.get(url, params={"format": "json", "query": SPARQL_query})
     data = response.json()
 
-    if not os.path.exists(results_path):
-        os.mkdir(results_path)
+    if not os.path.exists(query_results):
+        os.mkdir(query_results)
 
-    with open(f"{results_path}/{wdProperty}.json", "w") as outfile:
+    with open(f"{query_results}/{wdProperty}.json", "w") as outfile:
         json.dump(data, outfile, indent=2)
 
 
 def wdPropertyConstraints(wdProperty):
     property_constraints = []
-    with open(f"{results_path}/{wdProperty}.json", "r") as property_file:
+    with open(f"{query_results}/{wdProperty}.json", "r") as property_file:
         property_json = json.load(property_file)
         property_file.close()
         print(f"Property {wdProperty} has the following property constraints:")
@@ -76,47 +91,85 @@ def wdPropertyConstraints(wdProperty):
 def shacl_generate(wdProperty, property_json, property_constraints):
     for item in property_constraints:
         if item in constraint_shacl:
-            if item == "Q21503250":
-                constraint_shape = constraint_shacl[item]
-                constraint_shape = constraint_shape.replace("$WD_PROPERTY$", wdProperty)
-                for json_item in property_json["results"]["bindings"]:
-                    if (
-                        json_item.get("constraint_type").get("value")[
-                            json_item.get("constraint_type").get("value").rfind("/Q")
-                            + 1 :
-                        ]
-                        == item
-                        and json_item.get("pq_qualifiers").get("value")
-                        != "http://www.wikidata.org/prop/qualifier/P2309"
-                    ):
-                        value_list = (
-                            json_item.get("object_val").get("value").split(", ")
-                        )
-                        for value in value_list:
-                            property_shacl = """
-                            [ sh:property [
-                                    sh:path ([sh:zeroOrOnePath wdt:P31] [sh:zeroOrMorePath wdt:P279]) ;
-                                    sh:minCount 1 ;
-                                    sh:hasValue wd:$PROPERTY$ ;
-                                ]
+            match item:
+                case "Q21503250":
+                    results_path = "./type constraint"
+                    constraint_shape = constraint_shacl[item]
+                    constraint_shape = constraint_shape.replace(
+                        "$WD_PROPERTY$", wdProperty
+                    )
+                    for json_item in property_json["results"]["bindings"]:
+                        if (
+                            json_item.get("constraint_type").get("value")[
+                                json_item.get("constraint_type")
+                                .get("value")
+                                .rfind("/Q")
+                                + 1 :
                             ]
-                            $PROPERTY_SHACL$
-                            """
-                            property_shacl = property_shacl.replace(
-                                "$PROPERTY$", value[value.rfind("/Q") + 1 :]
+                            == item
+                            and json_item.get("pq_qualifiers").get("value")
+                            != "http://www.wikidata.org/prop/qualifier/P2309"
+                        ):
+                            value_list = (
+                                json_item.get("object_val").get("value").split(", ")
                             )
+                            for value in value_list:
+                                property_shacl = """
+                                [ sh:property [
+                                        sh:path ([sh:zeroOrOnePath wdt:P31] [sh:zeroOrMorePath wdt:P279]) ;
+                                        sh:minCount 1 ;
+                                        sh:hasValue wd:$PROPERTY$ ;
+                                    ]
+                                ]
+                                $PROPERTY_SHACL$
+                                """
+                                property_shacl = property_shacl.replace(
+                                    "$PROPERTY$", value[value.rfind("/Q") + 1 :]
+                                )
+                                constraint_shape = constraint_shape.replace(
+                                    "$PROPERTY_SHACL$", property_shacl
+                                )
                             constraint_shape = constraint_shape.replace(
-                                "$PROPERTY_SHACL$", property_shacl
+                                "$PROPERTY_SHACL$", ""
                             )
-                        constraint_shape = constraint_shape.replace("$PROPERTY_SHACL$", "")
-                with open(f"{results_path}/shacl_shape_{wdProperty}_{item}.ttl", "w") as outfile:
-                    outfile.write(constraint_shape)
-                    outfile.close()
+
+                    if not os.path.exists(results_path):
+                        os.mkdir(results_path)
+
+                    with open(f"{results_path}/{wdProperty}.ttl", "w") as outfile:
+                        outfile.write(constraint_shape)
+                        outfile.close()
+                case "Q21503247":
+                    pass
+                    # for json_item in property_json["results"]["bindings"]:
+                    #     if (
+                    #         json_item.get("constraint_type").get("value")[
+                    #             json_item.get("constraint_type")
+                    #             .get("value")
+                    #             .rfind("/Q")
+                    #             + 1 :
+                    #         ]
+                    #         == item
+                    #         and json_item.get("pq_qualifiers").get("value")
+                    #         == "http://www.wikidata.org/prop/qualifier/P2305"
+                    #     ):
+                    #         value_list = (
+                    #             json_item.get("object_val").get("value").split(", ")
+                    #         )
+                    #         print(value_list)
+                    # # results_path = "./item-requires-statement constraint"
+
+                    # # if not os.path.exists(results_path):
+                    # #     os.mkdir(results_path)
+
+                    # # with open(f"{results_path}/{wdProperty}.ttl", "w") as outfile:
+                    # #     outfile.write(constraint_shape)
+                    # #     outfile.close()
 
 
 # wdProperty = input(
 #     "For which property would you like to generate a SHACL property shape?\n> "
 # )
-wdProperty = "P1559"
+wdProperty = "P1469"
 wdQuery(wdProperty)
 wdPropertyConstraints(wdProperty)
